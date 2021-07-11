048444d4b6028ff2bc2f064452e49569f91c4fb4
thlanza <thlanza@hotmail.com>
2021-07-11 13:20:45 -0300

    first

---
+++ event-bus/index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const axios = require('axios');
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({
+    extended: true
+}));
+
+const events = [];
+
+app.post('/events', (req, res) => {
+    const event = req.body;
+
+    events.push(event);
+
+    axios.post('http://posts-clusterip-srv:4000/events', event).catch(err => console.log(err.message));
+    axios.post('http://comments-srv:4001/events', event).catch(err => console.log(err.message));
+    axios.post('http://query-srv:4002/events', event).catch(err => console.log(err.message));
+    axios.post('http://moderation-srv:4003/events', event).catch(err => console.log(err.message));;
+
+    res.send({ status: 'OK' });
+});
+
+app.get('/events', (req, res) => {
+    res.send(events);
+});
+
+app.listen(4005, () => {
+    console.log('Escutando na porta 4005.')
+});
